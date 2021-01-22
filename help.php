af472df78ecf0e48e61a71056b4eeaa49994c92b
Avishka217 <>
2021-01-22 12:40:08 +0530

    FAQ page

--- help.php
+++ help.php
@@ -3,12 +3,163 @@
 <html>
     <head>
         <title>EasyCart | Help</title>
-        <link rel="stylesheet" href="navbar.css">
+        <link rel="stylesheet" type="text/css" href="./styles/navbar.css">
+        <link rel="stylesheet" type="text/css" href="./styles/faqstyles.css">
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     </head>
     <body>
         <!-- Navbar -->
+
         <?php include('navbar.php'); ?>
+
+
+
+        <span style="font-size:70px; padding:0; border:0;color:#D4AF37;"><b>FAQ's </span>
+        <div id="Qcontainer" style="background: url('./images/faqback.jpg') no-repeat; background-size: 100%; " >
+
+
+            <div class="QaA">
+                      <div class="questionandimg">
+
+                                  <img class="qimg"src="./images/question.png" alt="" width="50px";>
+                                  <div class="question">
+                                      <p>How can I select the item category which I need ?</p>
+
+                                  </div>
+                      </div>
+
+                      <div class="clear">
+
+                      </div>
+
+                    <div class="answer">
+                        <p>The "Products" page has a filter option.
+                         Then select the desired category and click the "Filter" button.
+                          The system will then show you the items related to the category you want.</p>
+
+
+                    </div>
+                    <br>
+                    <hr class="ruler">
+            </div>
+
+
+
+            <div class="QaA">
+                      <div class="questionandimg">
+
+                                  <img class="qimg"src="./images/question.png" alt="" width="50px";>
+                                  <div class="question A">
+                                      <p>Is it mandatory for the card to be in my name when paying by using card payments method?</p>
+
+                                  </div>
+                      </div>
+
+
+                      <div class="clear">
+
+                      </div>
+                    <div class="answer">
+                              <p>
+                                  No.The card does not required to be in your name. You can make payments using someone else's card with the cardholder's permission. But you must be able to provide accurate information including the card number
+
+
+
+                              </p>
+
+                    </div>
+                    <br>
+                      <hr class="ruler">
+            </div>
+
+
+
+
+            <div class="QaA">
+                      <div class="questionandimg">
+
+                                  <img class="qimg"src="./images/question.png" alt="" width="50px";>
+                                  <div class="question">
+                                        <p>Where can I find items after I purchased them?</p>
+
+                                  </div>
+                      </div>
+
+
+                      <div class="clear">
+
+                      </div>
+                    <div class="answer">
+
+
+                          <p>Once you have provided the relevant information for the purchase of the product,
+                             the notification will go to the seller through the system so you do not have
+                             to worry about the procedure after the clicking the button "buy" in the cart.</p>
+
+
+                    </div>
+                    <br>
+                      <hr class="ruler">
+            </div>
+
+
+
+
+
+            <div class="QaA">
+                      <div class="questionandimg">
+
+                                  <img class="qimg"src="./images/question.png" alt="" width="50px";>
+                                  <div class="question">
+                                      <p>If an item is rejected for any reason, will that amount be refunded?</p>
+
+                                  </div>
+                      </div>
+
+                      <div class="clear">
+
+                      </div>
+
+                    <div class="answer">
+                            <p>
+                                  No refunds will be given for you. But you will be given a replacement for the damaged item. No additional delivery charges.
+
+                            </p>
+                    </div>
+                    <br>
+                      <hr class="ruler">
+            </div>
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+        <!-- Body  -->
+
+
+
+
+
+
+
+
+
+
+
+
 
         <!-- Footer -->
         <?php include('footer.php'); ?>
